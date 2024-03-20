d8b1b16f378d38edc2e164895865a035b9495da2
Ahmad Andi Rahmadi <ahmadandi022@gmail.com>
2024-03-20 07:50:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-51237e82-b946-423f-9e40-de7cd24e06b0/)
 
 
 echo "$curl_output" >> Update.tsx
